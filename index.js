c65d77aff26cb717fc973dfb345a662a9549ba3d
aminhl <amin.hlel@esprit.tn>
2023-01-29 17:29:02 +0100

    template_filling

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const http = require("http");
+const fs = require("fs");
+
+// Reading data from json file
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const dataObj = JSON.parse(data);
+
+// Importing templates
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`
+);
+
+// Function that replace the placeholders in the card template with the real data from the data json file
+const replaceTemplate = (template, product) => {
+  let output = template
+    .toString()
+    .replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.toString().replace(/{%IMAGE%}/g, product.image);
+  output = output.toString().replace(/{%PRICE%}/g, product.price);
+  output = output.toString().replace(/{%FROM%}/g, product.from);
+  output = output.toString().replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.toString().replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.toString().replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.toString().replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
 
 // Creating the server
 
@@ -6,7 +40,14 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
-    res.end("Welcome to overview");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const productCards = tempOverview
+      .toString()
+      .replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(productCards);
   } else if (pathName === "/product") {
     res.end("Welcome to product");
   } else {
